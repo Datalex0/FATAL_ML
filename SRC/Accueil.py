81bf52ca783adc9bdf0b26d9f3c0fd6a5963ceef
Alexis M <datalexis0@gmail.com>
2024-02-15 14:20:03 +0100

    Add CSV + pages

---
+++ SRC/Accueil.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import pandas as pd
+
+#Configuration des dimensions & affichage de la page
+st.set_page_config(page_title="Accueil", 
+                   page_icon=":clapper:", 
+                   layout='wide')
---
+++ SRC/pages/2- Machine Learning.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import pandas as pd
+
+#Configuration des dimensions & affichage de la page
+st.set_page_config(page_title="Machine Learning", 
+                   page_icon=":clapper:", 
+                   layout='wide')
